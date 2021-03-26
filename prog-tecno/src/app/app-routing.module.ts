db61717c4630b4089ab51112c1138f5ad95fd37d
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2021-03-26 11:52:01 +0000

    parte info

--- prog-tecno/src/app/app-routing.module.ts
+++ prog-tecno/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AssistenzaComponent } from './assistenza/assistenza.component';
+import { InformazioniComponent } from './informazioni/informazioni.component';
+const routes: Routes = [
+  { path: 'assistenza', component: AssistenzaComponent },
+  { path: 'informazioni', component: InformazioniComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
