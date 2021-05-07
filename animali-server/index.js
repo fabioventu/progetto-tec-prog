7d2ea848842af04315d67f07bb51f4c4f6749607
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2021-05-07 11:42:53 +0000

    immagini

--- animali-server/index.js
+++ animali-server/index.js
@@ -6,12 +6,12 @@ const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongod
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    const uri = "mongodb+srv:fabioventurini:ciaofiore@cluster0.zx7mf.mongodb.net/myFirstDatabase?"
+    const uri = "mongodb+srv:fabioventurini:ciaopimpa@cluster0.zx7mf.mongodb.net/myFirstDatabase?"
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         const collection = client.db("adottamy").collection("animals"); //Mi connetto alla collection movies
         // perform actions on the collection object
-        collection.find().limit(10).toArray((err, result) => {
+        collection.find().limit(20).toArray((err, result) => {
             if (err) console.log(err.message); //Se c'è qualche errore lo stampo
             else console.log(result);
             client.close(); //Quando ho terminato la find chiudo la sessione con il db
--- prog-tecno/src/app/informazioni/informazioni.component.ts
+++ prog-tecno/src/app/informazioni/informazioni.component.ts
@@ -9,6 +9,15 @@ export class InformazioniComponent implements OnInit {
 
   constructor() { }
 
+
+  image: any[] = [
+  "/assets/3NX63.jpg",
+  "/assets/4PTQ6.jpg",
+  "/assets/5C5QM.jpg",
+  "/assets/A2ZD8.jpg"
+ ]
+
+
   ngOnInit(): void {
   }
 
